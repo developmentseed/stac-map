47c2a72cfa6e032eaa2eb8625163d614b0fe687b
Pete Gadomski <pete.gadomski@gmail.com>
2025-09-09 15:33:58 -0700

    test: add one more

--- tests/app.spec.tsx
+++ tests/app.spec.tsx
@@ -1,15 +1,30 @@
-import { expect, test } from "vitest";
+import { beforeEach, expect, test } from "vitest";
 import { render } from "vitest-browser-react";
 import App from "../src/app";
 import { Provider } from "../src/components/ui/provider";
 
-test("renders example button", async () => {
-  const app = await render(
+async function renderApp() {
+  return await render(
     <Provider>
       <App></App>
     </Provider>,
   );
+}
+
+beforeEach(() => {
+  window.history.pushState({}, "", "/");
+});
+
+test("renders example button", async () => {
+  const app = await renderApp();
   await expect
     .element(app.getByRole("button", { name: "examples" }))
     .toBeVisible();
+});
+
+test("loads STAC API from eoapi.dev", async () => {
+  window.history.pushState({}, "", "?href=https://stac.eoapi.dev/");
+  const app = await renderApp();
+  await expect.element(app.getByText(/eoAPI-stac/i)).toBeVisible();
+  await expect.element(app.getByText(/collections/i)).toBeVisible();
 });
