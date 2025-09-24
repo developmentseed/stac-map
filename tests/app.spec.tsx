7d209ab2b899c5c4219b8358101a0d3bec29c119
Pete Gadomski <pete.gadomski@gmail.com>
2025-09-24 08:46:54 -0600

    refactor: use describe for tests

--- tests/app.spec.tsx
+++ tests/app.spec.tsx
@@ -1,9 +1,10 @@
-import { beforeEach, expect, test } from "vitest";
+import { describe, expect, test } from "vitest";
 import { render } from "vitest-browser-react";
 import App from "../src/app";
 import { Provider } from "../src/components/ui/provider";
 
-function renderApp() {
+function renderApp(path?: string) {
+  window.history.pushState({}, "", path || "");
   return render(
     <Provider>
       <App></App>
@@ -11,57 +12,52 @@ function renderApp() {
   );
 }
 
-beforeEach(() => {
-  window.history.pushState({}, "", "/");
+describe("initial state", () => {
+  test("renders example button", async () => {
+    const app = renderApp();
+    await expect
+      .element(app.getByRole("button", { name: "examples" }))
+      .toBeVisible();
+  });
 });
 
-test("renders example button", async () => {
-  const app = renderApp();
-  await expect
-    .element(app.getByRole("button", { name: "examples" }))
-    .toBeVisible();
+describe("loading", () => {
+  test("stac.eoapi.dev", async () => {
+    const app = renderApp("?href=https://stac.eoapi.dev/");
+    await expect.element(app.getByText(/eoAPI-stac/i)).toBeVisible();
+    await expect.element(app.getByText(/collections/i)).toBeVisible();
+
+    await app.getByRole("heading", { name: "Afghanistan Earthquake" }).click();
+    expect(new URL(window.location.href).search).toBe(
+      "?href=https://stac.eoapi.dev/collections/MAXAR_afghanistan_earthquake22",
+    );
+  });
+
+  test("CSDA Planet", async () => {
+    // https://github.com/developmentseed/stac-map/issues/96
+    const app = renderApp(
+      "?href=https://csdap.earthdata.nasa.gov/stac/collections/planet",
+    );
+    await expect
+      .element(app.getByRole("heading", { name: "Planet" }))
+      .toBeVisible();
+  });
+
+  test("NAIP stac-geoparquet", async () => {
+    const app = renderApp(
+      "?href=https://raw.githubusercontent.com/developmentseed/labs-375-stac-geoparquet-backend/refs/heads/main/data/naip.parquet",
+    );
+    await expect.element(app.getByText(/stac-geoparquet/i)).toBeVisible();
+  });
 });
 
-test("loads stac.eoapi.dev", async () => {
-  window.history.pushState({}, "", "?href=https://stac.eoapi.dev/");
-  const app = renderApp();
-  await expect.element(app.getByText(/eoAPI-stac/i)).toBeVisible();
-  await expect.element(app.getByText(/collections/i)).toBeVisible();
-
-  await app.getByRole("heading", { name: "Afghanistan Earthquake" }).click();
-  expect(new URL(window.location.href).search).toBe(
-    "?href=https://stac.eoapi.dev/collections/MAXAR_afghanistan_earthquake22",
-  );
-});
-
-test("loads CSDA Planet", async () => {
-  // https://github.com/developmentseed/stac-map/issues/96
-  window.history.pushState(
-    {},
-    "",
-    "?href=https://csdap.earthdata.nasa.gov/stac/collections/planet",
-  );
-  const app = renderApp();
-  await expect
-    .element(app.getByRole("heading", { name: "Planet" }))
-    .toBeVisible();
-});
-
-test("loads NAIP stac-geoparquet", async () => {
-  window.history.pushState(
-    {},
-    "",
-    "?href=https://raw.githubusercontent.com/developmentseed/labs-375-stac-geoparquet-backend/refs/heads/main/data/naip.parquet",
-  );
-  const app = renderApp();
-  await expect.element(app.getByText(/stac-geoparquet/i)).toBeVisible();
-});
-
-test("navigates through a static catalog", async () => {
-  const app = renderApp();
-  await app.getByRole("button", { name: "Examples" }).click();
-  await app.getByRole("menuitem", { name: "Maxar Open Data static" }).click();
-  await app.getByText("Bay of Bengal Cyclone Mocha").click();
-  await app.getByText("10300100E6747500", { exact: true }).click();
-  // TODO test map clicking, oof
+describe("navigation", () => {
+  test("static catalog", async () => {
+    const app = renderApp();
+    await app.getByRole("button", { name: "Examples" }).click();
+    await app.getByRole("menuitem", { name: "Maxar Open Data static" }).click();
+    await app.getByText("Bay of Bengal Cyclone Mocha").click();
+    await app.getByText("10300100E6747500", { exact: true }).click();
+    // TODO test map clicking, oof
+  });
 });
