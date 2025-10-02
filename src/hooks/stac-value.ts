91e3f2fb5b26eb5b000f9e103f3d369f20458050
Pete Gadomski <pete.gadomski@gmail.com>
2025-10-02 14:41:05 -0600

    fix: infinite update

--- src/hooks/stac-value.ts
+++ src/hooks/stac-value.ts
@@ -38,8 +38,6 @@ export default function useStacValue({
         }
         setConnection(connection);
       })();
-    } else {
-      setConnection(undefined);
     }
   }, [db, href, fileUpload]);
 
