8617f9f22abbf9de153e3b41a90927d744f9d4ce
Pete Gadomski <pete.gadomski@gmail.com>
2025-10-27 07:10:04 -0600

    fix: add an error boundary for each section

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -1,6 +1,7 @@
 import { type ReactNode } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 import { type IconType } from "react-icons/lib";
-import { Accordion, HStack, Icon } from "@chakra-ui/react";
+import { Accordion, Alert, HStack, Icon } from "@chakra-ui/react";
 
 export default function Section({
   title,
@@ -25,8 +26,24 @@ export default function Section({
         <Accordion.ItemIndicator />
       </Accordion.ItemTrigger>
       <Accordion.ItemContent>
-        <Accordion.ItemBody>{children}</Accordion.ItemBody>
+        <Accordion.ItemBody>
+          <ErrorBoundary FallbackComponent={FallbackComponent}>
+            {children}
+          </ErrorBoundary>
+        </Accordion.ItemBody>
       </Accordion.ItemContent>
     </Accordion.Item>
+  );
+}
+
+function FallbackComponent({ error }: { error: Error }) {
+  return (
+    <Alert.Root status={"error"}>
+      <Alert.Indicator />
+      <Alert.Content>
+        <Alert.Title>An error occurred during rendering</Alert.Title>
+        <Alert.Description>{error.message}</Alert.Description>
+      </Alert.Content>
+    </Alert.Root>
   );
 }
