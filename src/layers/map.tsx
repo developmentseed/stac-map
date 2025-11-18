1e6e70c2e85a43c743c1229279361eaa7e708636
Sandra Hoang <sandrahoang686@gmail.com>
2025-11-18 11:36:25 -0500

    check if bbox is a list of lists

--- src/layers/map.tsx
+++ src/layers/map.tsx
@@ -263,8 +263,10 @@ function valueToGeoJson(value: StacValue) {
   }
 }
 
-function getCollectionExtents(collection: StacCollection) {
-  return collection.extent?.spatial?.bbox?.[0];
+function getCollectionExtents(collection: StacCollection): SpatialExtent {
+  const spatialExtent = collection.extent?.spatial;
+  // check if bbox is a list of lists, otherwise its a single list of nums
+  return Array.isArray(spatialExtent?.bbox?.[0]) ? spatialExtent?.bbox[0] : ((spatialExtent?.bbox as unknown) as SpatialExtent);
 }
 
 function getBbox(
