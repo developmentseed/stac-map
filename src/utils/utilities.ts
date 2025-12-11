9637c03a925547ef6181a6b09ff881111396ba6d
Sandra Hoang <sandrahoang686@gmail.com>
2025-12-11 16:44:50 -0500

    remove code note

--- src/utils/utilities.ts
+++ src/utils/utilities.ts
@@ -1,7 +1,5 @@
 import type { StacValue } from "../types/stac";
 
-// @NOTE-SANDRA: Turn this into a simple function instead
-// the useEffect isn't really needed at this level because only app.tsx needs to subscribe to it
 export default function setDocumentTitle(value: StacValue | undefined) {
   let title = "stac-map";
   if (value && (value.title || value.id)) {
