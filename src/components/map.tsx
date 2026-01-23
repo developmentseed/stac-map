7c6652cf15430e53d187da0e39e9171538456b13
Pete Gadomski <pete.gadomski@gmail.com>
2026-01-23 12:57:47 -0700

    fix: bounding

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -356,7 +356,7 @@ function getBbox(
   if (items && items.length > 0) {
     return sanitizeBbox(bbox(featureCollection(items)) as BBox2D);
   }
-  if (!value) {
+  if (!value || stacGeoparquetItemId) {
     return undefined;
   }
   let valueBbox;
