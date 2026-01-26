139b7f022a4de6fe49c0bacda404036a9fa6530f
Pete Gadomski <pete.gadomski@gmail.com>
2026-01-26 06:23:03 -0700

    fix: tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,11 @@
 import react from "@vitejs/plugin-react";
 import { playwright } from "@vitest/browser-playwright";
 import wasm from "vite-plugin-wasm";
+import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  plugins: [react(), wasm()],
+  plugins: [react(), tsconfigPaths(), wasm()],
   optimizeDeps: {
     include: ["@deck.gl/core", "@duckdb/duckdb-wasm", "react-icons/lib"],
   },
