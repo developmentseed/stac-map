7cf5922bdd26ffaa91942a374c0cdbb98bcef269
Pete Gadomski <pete.gadomski@gmail.com>
2026-02-05 07:40:42 -0700

    fix: add footer to collection cards

--- src/components/cards/collection.tsx
+++ src/components/cards/collection.tsx
@@ -1,4 +1,5 @@
 import { useStore } from "@/store";
+import { Text, VStack } from "@chakra-ui/react";
 import type { StacCollection } from "stac-ts";
 import ValueCard from "./value";
 
@@ -10,6 +11,26 @@ export default function CollectionCard({
   const hoveredCollection = useStore((store) => store.hoveredCollection);
   const setHoveredCollection = useStore((store) => store.setHoveredCollection);
 
+  const bbox = collection.extent?.spatial?.bbox?.[0];
+  const interval = collection.extent?.temporal?.interval?.[0];
+
+  const formatBbox = (bbox: number[]) => {
+    return bbox.map((v) => v.toFixed(2)).join(", ");
+  };
+
+  const formatInterval = (interval: (string | null)[]) => {
+    const start = interval[0] ? interval[0].split("T")[0] : "..";
+    const end = interval[1] ? interval[1].split("T")[0] : "..";
+    return `${start} / ${end}`;
+  };
+
+  const footer = (bbox || interval) && (
+    <VStack align="start" gap={0} fontSize="xs" color="fg.muted">
+      {bbox && <Text>Bbox: {formatBbox(bbox)}</Text>}
+      {interval && <Text>Temporal: {formatInterval(interval)}</Text>}
+    </VStack>
+  );
+
   return (
     <ValueCard
       value={collection}
@@ -18,6 +39,7 @@ export default function CollectionCard({
       onMouseLeave={() => {
         if (hoveredCollection?.id === collection.id) setHoveredCollection(null);
       }}
+      footer={footer}
     />
   );
 }
