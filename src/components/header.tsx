1aca2752cc6177eebc5a3d94111270cb4d9eee04
Pete Gadomski <pete.gadomski@gmail.com>
2026-02-05 05:40:42 -0700

    fix: one fewer button

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,4 @@
-import { Button, FileUpload, HStack, IconButton } from "@chakra-ui/react";
-import { useDuckDb } from "duckdb-wasm-kit";
-import { LuUpload } from "react-icons/lu";
-import { useStore } from "../store";
-import { uploadFile } from "../utils/upload";
+import { Button, HStack } from "@chakra-ui/react";
 import { Examples } from "./examples";
 import HrefInput from "./href-input";
 import { ColorModeButton } from "./ui/color-mode";
@@ -10,29 +6,9 @@ import { ProjectionButton } from "./ui/projection";
 import { SettingsButton } from "./ui/settings";
 
 export default function Header() {
-  const setUploadedFile = useStore((store) => store.setUploadedFile);
-  const { db } = useDuckDb();
-
   return (
     <HStack pointerEvents={"auto"}>
       <HrefInput />
-      <FileUpload.Root
-        flex={0}
-        onFileAccept={(details) =>
-          uploadFile({
-            file: details.files[0],
-            setUploadedFile,
-            db,
-          })
-        }
-      >
-        <FileUpload.HiddenInput />
-        <FileUpload.Trigger asChild>
-          <IconButton bg={"bg.muted/90"} variant={"outline"} disabled={!db}>
-            <LuUpload />
-          </IconButton>
-        </FileUpload.Trigger>
-      </FileUpload.Root>
       <Examples>
         <Button bg={"bg.muted/90"} variant={"outline"}>
           Examples
--- src/components/href-input.tsx
+++ src/components/href-input.tsx
@@ -1,10 +1,21 @@
-import { Box, Input } from "@chakra-ui/react";
+import {
+  Box,
+  FileUpload,
+  IconButton,
+  Input,
+  InputGroup,
+} from "@chakra-ui/react";
+import { useDuckDb } from "duckdb-wasm-kit";
+import { LuUpload } from "react-icons/lu";
 import { useStore } from "../store";
+import { uploadFile } from "../utils/upload";
 
 export default function HrefInput() {
   const setHref = useStore((state) => state.setHref);
   const input = useStore((state) => state.input);
   const setInput = useStore((state) => state.setInput);
+  const setUploadedFile = useStore((store) => store.setUploadedFile);
+  const { db } = useDuckDb();
 
   return (
     <Box
@@ -15,12 +26,33 @@ export default function HrefInput() {
       }}
       flex="1"
     >
-      <Input
-        bg={"bg.muted/90"}
-        placeholder="Enter a url to a STAC API, JSON, or GeoParquet"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      ></Input>
+      <InputGroup
+        endElement={
+          <FileUpload.Root
+            onFileAccept={(details) =>
+              uploadFile({
+                file: details.files[0],
+                setUploadedFile,
+                db,
+              })
+            }
+          >
+            <FileUpload.HiddenInput />
+            <FileUpload.Trigger asChild>
+              <IconButton variant={"plain"} size={"sm"} disabled={!db}>
+                <LuUpload />
+              </IconButton>
+            </FileUpload.Trigger>
+          </FileUpload.Root>
+        }
+      >
+        <Input
+          bg={"bg.muted/90"}
+          placeholder="Enter a url to a STAC API, JSON, or GeoParquet"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </InputGroup>
     </Box>
   );
 }
