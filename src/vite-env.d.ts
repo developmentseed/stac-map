99697d6545d1aa41ed16cd21cdf2636fa59e0a53
Pete Gadomski <pete.gadomski@gmail.com>
2026-04-30 10:46:29 -0600

    fix: deps

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,7 +5,6 @@ interface ImportMetaEnv {
   readonly VITE_AUTH_AUTHORITY?: string;
   readonly VITE_AUTH_CLIENT_ID?: string;
   readonly VITE_STAC_BROWSER_URL?: string;
-  readonly VITE_STAC_NATURAL_QUERY_API?: string;
 }
 
 interface ImportMeta {
