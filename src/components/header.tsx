f6d638411b95eafc204e3c39901c3e503a4340e9
Pete Gadomski <pete.gadomski@gmail.com>
2026-05-06 08:42:39 -0600

    feat: add a simple share link (#449)
    Right now it's just the map bbox, but we'll add more

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,9 @@
-import { Button, ButtonGroup, HStack } from "@chakra-ui/react";
+import { useStore } from "@/store";
+import { getPaddedViewportBbox } from "@/utils/map";
+import { Button, ButtonGroup, HStack, IconButton } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { LuCheck, LuShare2 } from "react-icons/lu";
+import { useMap } from "react-map-gl/maplibre";
 import { useAuthEnabled } from "../contexts/auth-enabled";
 import { useExamples } from "../contexts/examples";
 import { Examples } from "./examples";
@@ -9,22 +14,50 @@ import { ProjectionButton } from "./ui/projection";
 import { SettingsButton } from "./ui/settings";
 
 export default function Header() {
+  const href = useStore((store) => store.href);
   const authEnabled = useAuthEnabled();
   const examples = useExamples();
   return (
     <HStack pointerEvents={"auto"}>
       <HrefInput />
+      {href?.startsWith("http") && <ShareButton />}
+      {examples.length > 0 && (
+        <Examples>
+          <Button bg={"bg.muted/90"} variant={"surface"}>
+            Examples
+          </Button>
+        </Examples>
+      )}
       <ButtonGroup variant={"surface"} attached>
-        {examples.length > 0 && (
-          <Examples>
-            <Button bg={"bg.muted/90"}>Examples</Button>
-          </Examples>
-        )}
         <ProjectionButton />
         <ColorModeButton />
         <SettingsButton />
         {authEnabled && <UserButton />}
       </ButtonGroup>
     </HStack>
+  );
+}
+
+function ShareButton() {
+  const { map } = useMap();
+  const [copied, setCopied] = useState(false);
+
+  useEffect(() => {
+    if (!copied) return;
+    const id = setTimeout(() => setCopied(false), 3000);
+    return () => clearTimeout(id);
+  }, [copied]);
+
+  async function copyShareUrl() {
+    const url = new URL(window.location.href);
+    if (map) url.searchParams.set("bbox", getPaddedViewportBbox(map).join(","));
+    await navigator.clipboard.writeText(url.toString());
+    setCopied(true);
+  }
+
+  return (
+    <IconButton variant={"surface"} bg={"bg.muted/90"} onClick={copyShareUrl}>
+      {copied ? <LuCheck /> : <LuShare2 />}
+    </IconButton>
   );
 }
